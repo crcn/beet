966c09a6f16874570c5b346c3ed7fdf4dc38857a
Craig Condon <craig@spiceapps.com>
2011-09-16 20:44:04 -0500

    removed more junk

--- app/server/index.js
+++
@@ -1,6 +0,0 @@
-var beanpole = require('beanpole');
-
-beanpole.params({'express.core':{ port: 8730 }}).
-require(['hook.core','hook.http.mesh']).
-require(__dirname + '/beans').
-push('init', '/data/beet/scripts.db');
